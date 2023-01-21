f9f1758897b77bca9622a118764ff7e8ded111f6
Lynmony Puth <puthlynmony@gmail.com>
2023-01-21 18:13:52 +0700

    Initial Commit

---
+++ SwiftUICrypto/SwiftUICryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICryptoApp.swift
+//  SwiftUICrypto
+//
+//  Created by Lynmony Puth on 21/1/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICryptoApp: App {
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+}
